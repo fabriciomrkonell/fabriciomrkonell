a154029c035ce9dfba6cc94e83ead153fdfb1954
Fabrício Ronchi <fabricio_konell@hotmail.com>
2014-03-26 16:47:32 -0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,15 @@
 $(function(){
-  $(".contato-icon").hover(function(){
+  $(".contato-icon").hover(function(event){
     var atual = $(this).attr("data-status");
       if(atual == 1){
         $("#label-contato").html("Follow me on Twitter!");
+        return true;
       }else if(atual ==2){
         $("#label-contato").html("Add me on Linkedin!");
+        return false;
       }else{
         $("#label-contato").html("Follow me on GitHub!");
+        event.preventDefault();
       }
     }, function(){
         $("#label-contato").html("")
