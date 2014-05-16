3f27057757090f187b4b1786aa3bbf044e471732
Desenvolvimento <fabricioronchii@gmail.com>
2014-05-16 15:27:41 -0300

    Post Livro

--- js/javascript.js
+++ js/javascript.js
@@ -5,6 +5,8 @@ $(function(){
         $("#label-contato").html("Follow me on Twitter!");
       }else if(atual ==2){
         $("#label-contato").html("Add me on Linkedin!");
+      }else if (atual == 3){
+        $("#label-contato").html("Follow me on Instagram!");
       }else{
         $("#label-contato").html("Follow me on GitHub!");
       }
