1635aa07b85deb70d68eeab0877f56a3428b6146
Desenvolvimento <fabricioronchii@gmail.com>
2014-07-30 20:00:01 -0300

    Correção

--- js/javascript.js
+++ js/javascript.js
@@ -3,9 +3,7 @@ $(function(){
     var atual = $(this).attr("data-status");
       if(atual == 1){
         $("#label-contato").html("Follow me on Twitter!");
-      }else if(atual ==2){
-        $("#label-contato").html("Add me on Linkedin!");
-      }else if (atual == 3){
+      }else if (atual == 2){
         $("#label-contato").html("Follow me on Instagram!");
       }else {
         $("#label-contato").html("Follow me on GitHub!");
