1ab3ef225c60bc6064b3c7a2008c9dcb168b5645
Desenvolvimento <fabricioronchii@gmail.com>
2014-08-16 08:13:14 -0300

    Teste

--- js/arduino/arduino.js
+++ js/arduino/arduino.js
@@ -1,3 +1,5 @@
 $(function(){
-  alert();
+  $.ajax( "https://www.docs.google.com/forms/d/1MjHhY40Hz__lBvUKTaVupRK3B7O9UFeiVcqqhyipJio/formResponse?entry.1795253174=999&entry.2125948701=91" ).done(function() {
+    alert( "success" );
+  });
 });
