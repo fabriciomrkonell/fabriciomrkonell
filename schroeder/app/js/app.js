b810db064f83b85e200b7505a5d9d280b9e533b7
Desenvolvimento <fabricioronchii@gmail.com>
2014-08-16 14:46:34 -0300

    Schroeder

--- schroeder/app/js/app.js
+++ schroeder/app/js/app.js
@@ -1,9 +1,9 @@
 var app = angular.module('schroederApp', []);
 
 app.controller("schroederCtrl", ["$scope", "$http", function($scope, $http){
-	
+
 	$scope.page = 1;
-	$scope.model = {		
+	$scope.model = {
 		temperaturas: [],
 		medidas: 0,
 		media:  {
@@ -21,56 +21,57 @@ app.controller("schroederCtrl", ["$scope", "$http", function($scope, $http){
 		minima: {
 			temperatura: "0",
 			humidade: "0"
-		}		
+		}
 	};
-	
+
 	$scope.goPage = function(page){
 		$scope.page = page;
 	};
-	
+
 	$scope.getData = function(data){
-		return data.gsx$indicaçãodedataehora.$t.split(" ")[1] + " - " + data.gsx$indicaçãodedataehora.$t.split(" ")[0];
+		return "0";
+		//return data.gsx$indicaçãodedataehora.$t.split(" ")[1] + " - " + data.gsx$indicaçãodedataehora.$t.split(" ")[0];
 	};
-	
+
 	$scope.getDataMaxima = function(data){
-		if(angular.isObject(data) == true){
+		/*if(angular.isObject(data) == true){
 			return data.gsx$indicaçãodedataehora.$t.split(" ")[0];
-		}
+		}*/
 		return 0;
 	};
-		
+
 	$http({
-		method: 'GET', 
-		url: "https://spreadsheets.google.com/feeds/list/1BH0BEaVd7WMWNXDTCoiFmDu_ABmrh_5cdPE5wqT7qvI/1/public/values?alt=json"
+		method: 'GET',
+		url: "http://pesagem.ranchobom.com/Danger/schroeder"
 	}).success(function(data, status, headers, config) {
-			$scope.model.temperaturas = data.feed.entry;
-			$scope.model.sensor.temperatura = data.feed.entry[data.feed.entry.length - 1].gsx$temperatura.$t;			
-			$scope.model.sensor.humidade = data.feed.entry[data.feed.entry.length - 1].gsx$humidade.$t;
-			$scope.model.minima.temperatura = data.feed.entry[0];
-			$scope.model.maxima.temperatura = data.feed.entry[0];
-			$scope.model.minima.humidade = data.feed.entry[0];
-			$scope.model.maxima.humidade = data.feed.entry[0];
-			$scope.model.medidas = data.feed.entry.length;
-      for(var i = 0; i < data.feed.entry.length; i++){
-      				$scope.model.media.temperatura = $scope.model.media.temperatura + parseInt(data.feed.entry[i].gsx$temperatura.$t);
-      				$scope.model.media.humidade = $scope.model.media.humidade + parseInt(data.feed.entry[i].gsx$humidade.$t);
-				if(parseInt(data.feed.entry[i].gsx$temperatura.$t) > parseInt($scope.model.maxima.temperatura.gsx$temperatura.$t)){
-					$scope.model.maxima.temperatura = data.feed.entry[i];
-				};	
-				if(parseInt(data.feed.entry[i].gsx$humidade.$t) > parseInt($scope.model.maxima.humidade.gsx$humidade.$t)){
-					$scope.model.maxima.humidade = data.feed.entry[i];
+			$scope.model.temperaturas = data;
+			$scope.model.sensor.temperatura = data[data.length - 1].temperatura;
+			$scope.model.sensor.humidade = data.[data.length - 1].humidade;
+			$scope.model.minima.temperatura = data[0];
+			$scope.model.maxima.temperatura = data[0];
+			$scope.model.minima.humidade = data[0];
+			$scope.model.maxima.humidade = data[0];
+			$scope.model.medidas = data.length;
+      for(var i = 0; i < data.length; i++){
+      				$scope.model.media.temperatura = $scope.model.media.temperatura + parseInt(data[i].temperatura);
+      				$scope.model.media.humidade = $scope.model.media.humidade + parseInt(data[i].humidade);
+				if(parseInt(data[i].temperatura) > parseInt($scope.model.maxima.temperatura.temperatura)){
+					$scope.model.maxima.temperatura = data[i];
 				};
-				if(parseInt(data.feed.entry[i].gsx$temperatura.$t) < parseInt($scope.model.minima.temperatura.gsx$temperatura.$t)){
-					$scope.model.minima.temperatura = data.feed.entry[i];
+				if(parseInt(data[i].humidade) > parseInt($scope.model.maxima.humidade.humidade)){
+					$scope.model.maxima.humidade = data[i];
 				};
-				if(parseInt(data.feed.entry[i].gsx$humidade.$t) < parseInt($scope.model.minima.humidade.gsx$humidade.$t)){
-					$scope.model.minima.humidade = data.feed.entry[i];
+				if(parseInt(data[i].temperatura) < parseInt($scope.model.minima.temperatura.temperatura)){
+					$scope.model.minima.temperatura = data[i];
+				};
+				if(parseInt(data.[i].humidade) < parseInt($scope.model.minima.humidade.humidade)){
+					$scope.model.minima.humidade = data[i];
 				};
 			};
 
-			
+
 	}).error(function(data, status, headers, config) {
 		window.location.reload();
   });
-	
+
 }]);
