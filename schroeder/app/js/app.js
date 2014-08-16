2cd76a4305496086c617b42db4db3082e27eeb2d
Desenvolvimento <fabricioronchii@gmail.com>
2014-08-16 15:05:03 -0300

    Schroeder

--- schroeder/app/js/app.js
+++ schroeder/app/js/app.js
@@ -40,8 +40,7 @@ app.controller("schroederCtrl", ["$scope", "$http", function($scope, $http){
 		return 0;
 	};
 
-	$http.get({url: "http://pesagem.ranchobom.com/Danger/schroeder"
-	}).success(function(data, status, headers, config) {
+	$http.get("http://pesagem.ranchobom.com/Danger/schroeder").success(function(data, status, headers, config) {
 			$scope.model.temperaturas = data;
 			$scope.model.sensor.temperatura = data[data.length - 1].temperatura;
 			$scope.model.sensor.humidade = data[data.length - 1].humidade;
